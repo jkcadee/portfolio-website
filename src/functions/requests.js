b6201a83fdb5acd827602e0853e4bf46af99dc99
jkcadee <54956049+jkcadee@users.noreply.github.com>
2021-01-08 17:06:13 -0800

    removing env variable

--- src/functions/requests.js
+++ src/functions/requests.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const contactUrl = process.env.REACT_APP_CONTACT_URL;
+// const contactUrl = process.env.REACT_APP_CONTACT_URL;
 
-console.log(contactUrl);
-
-export const sendContactMessage = (payload) => axios.post(contactUrl, payload);
+export const sendContactMessage = (payload) => axios.post("https://301bmpbe8a.execute-api.us-east-1.amazonaws.com/Test/contact_email_resource", payload);
